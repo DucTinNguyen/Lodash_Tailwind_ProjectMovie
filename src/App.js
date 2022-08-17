a7049811ace4d6ee9f995b73c0c1fbf4ee6287cb
Ductin07 <Tinn948@gmail.com>
2022-08-17 17:08:36 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import DemoGrid from './Tailwindcss/DemoGrid';
-import DemoFlex from './Tailwindcss/DemoFlex';
-import JoinDemo from './Lodash/JoinDemo';
-import FirstLastMethod from './Lodash/FirstLastMethod';
-import ChunkDemo from './Lodash/ChunkDemo';
-import SortDemo from './Lodash/SortDemo';
-import FlattenLodash from './Lodash/FlattenLodash';
-import CompareLodash from './Lodash/CompareLodash';
 
 function App() {
   return (
     
-      // <DemoGrid />
-      // <DemoFlex />
-      // <JoinDemo />
-      // <FirstLastMethod />
-      // <ChunkDemo />
-      // <SortDemo />
-      // <FlattenLodash />
-      <CompareLodash />
+      <div>
+
+      </div>
 
   )
 }
